c2f3f62398eb6dfa97a1f7bc8d074e5906b5ac47
Landan <102269895+LandanJH@users.noreply.github.com>
2023-07-20 11:36:12 -0500

    Update DCC2_Cleanser.py
    Final?

--- DCC2_Cleanser.py
+++ DCC2_Cleanser.py
@@ -3,14 +3,20 @@ import re
 import argparse
 
 def list_to_file(list, name):
+#Function to take a list and puth the list in a file line-by-line
     file = open( name+'.cleaned', 'w')
     for items in list:
         file.write(items+"\n")
     file.close()
     print('Cleaning finished output will be in the file named', name+'.cleaned')
 
-def File_Clenser(input):
+def Rem_Duplicates(hashes):
+# Function ot remove the duplicates from a list
+    hashes = list(dict.fromkeys(hashes))
+    return (hashes)
 
+def File_Clenser(input):
+#Function to go through a file of DCC2 hashes and clean them up
     hash_list = []          # list of uncleaned hashes
     cleaned_hashes = []     # list of cleaned hashes
     tmp_hashes = []         # temporary placeholder
@@ -32,8 +38,8 @@ def File_Clenser(input):
                 for item in tmp_hashes              
             ]
         # go through list and remove the duplicates
-        cleaned_hashes = list(dict.fromkeys(cleaned_hashes))
-        # closing the file and printing out the filteres hashes
+        cleaned_hashes = Rem_Duplicates(cleaned_hashes)
+        # closing the file
         file.close()
         # send the list to the 'list_to_file' function
         list_to_file(cleaned_hashes, input)
@@ -42,6 +48,7 @@ def File_Clenser(input):
         print('file does NOT exist')
 
 def Secrets_Parser(input, pattern):
+# Function to go through secrets folders and pull hashes
     hashes = []
     if (os.path.exists(input) == True):
         # open the file and starting to retrieve the DCC2 and NTLM hashes
@@ -56,7 +63,7 @@ def Secrets_Parser(input, pattern):
     return hashes
 
 def List_Cleanser (hash_list, input):
-
+# Function to clean a list of dcc2 hashes
     tmp_hashes = []         # temporary placeholder
 
     for x in range(len(hash_list)):
@@ -69,17 +76,33 @@ def List_Cleanser (hash_list, input):
             for item in tmp_hashes              
         ]
     # go through list and remove the duplicates
-    cleaned_hashes = list(dict.fromkeys(cleaned_hashes))
-    # closing the file and printing out the filteres hashes
+    cleaned_hashes = Rem_Duplicates(cleaned_hashes)
     # send the list to the 'list_to_file' function
     list_to_file(cleaned_hashes, input)
+
+def Directory_Parser(pattern, directory, mode):
+    hashes2 = []
+    fileslist =  os.listdir(directory)
+    for file in fileslist:
+        file_path = os.path.join(directory, file)
+        if os.path.isfile(file_path):
+            #print (file_path)
+            hashes = Secrets_Parser(file_path, pattern)
+            hashes2 = hashes + hashes2
+            print(hashes2)
+        if (mode == 'DCC2'):
+            List_Cleanser(hashes2, 'secretdumps.DCC2')
+        else:
+            hashes2 = Rem_Duplicates(hashes2)
+            list_to_file(hashes, 'sectretsdump.NTLM')
+
     
 if __name__ == "__main__":
     # Argument parcer stuff
     parser = argparse.ArgumentParser(description='Hash cleaner for the Domain Caches Credentials 2 hash type')
     parser.parse_args
     parser.add_argument('-f', '--FILE', type=str, help='File path of the file with the uncleaned DCC2 hashes', default=None)
-#    parser.add_argument('-d', '--DIRECTORY', type=str, help='Directory path of the files with the uncleaned DCC2 hashes', default=None)
+    parser.add_argument('-d', '--DIRECTORY', type=str, help='Directory path of the files with the uncleaned DCC2 hashes', default=None)
     parser.add_argument('-s', '--SECRETS', type=str, help='Path of the file with the secret dump hashes', default=None)
     parser.add_argument('-m', '--MODE', type=str, help='Type of hash to be found from secretsdump files DCC2 or NTLM', default=None)
     args = parser.parse_args()
@@ -88,19 +111,24 @@ if __name__ == "__main__":
     pattern_DCC2 = r"(\$DCC2\$)(.*)"
     pattern_NTLM = r"(.*?):(.*?):(.*?):(.*?):::"
 
-    if (args.FILE == None and args.SECRETS == None):
-        raise Exception('Please be sure to include the filename')
+    if (args.FILE == None and args.SECRETS == None and args.DIRECTORY == None):
+        print('Please be sure to include the filename: -f [FILENAME]')
     elif(args.SECRETS != None):
         if (args.MODE == 'DCC2'):
             hashes = Secrets_Parser(args.SECRETS, pattern_DCC2 )
-            List_Cleanser(hashes, 'secretdumps')
+            List_Cleanser(hashes, 'secretdumps.DCC2')
         elif (args.MODE == 'NTLM'):
             hashes = Secrets_Parser(args.SECRETS, pattern_NTLM )
-            List_Cleanser(hashes, 'secretdumps')
+            hashes = Rem_Duplicates(hashes)
+            list_to_file(hashes, 'sectretsdump.NTLM')
+        else:
+            print('Please specify the mode: -m [DCC2 / NTLM]')
+    elif(args.DIRECTORY != None):
+        if (args.MODE == 'DCC2'):
+            Directory_Parser(pattern_DCC2, args.DIRECTORY, 'DCC2')
+        elif (args.MODE == 'NTLM'):
+            Directory_Parser(pattern_NTLM, args.DIRECTORY, 'NTLM')
         else:
             print('Please specify the mode: -m [DCC2 / NTLM]')
     else:
-        # taking in the file name
-        #print('Please enter the file name:')
-        #input = input()
         File_Clenser(args.FILE)
