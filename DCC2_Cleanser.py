059e8f18e6cfc4624cefa5ee18f4f1e3fba3b65d
Landan <102269895+LandanJH@users.noreply.github.com>
2023-08-17 15:24:32 -0500

    Update DCC2_Cleanser.py
    Most of the code is now working, just working on some of the matching functions and should be working

--- DCC2_Cleanser.py
+++ DCC2_Cleanser.py
@@ -1,46 +1,42 @@
 import os
 import re
 import argparse
+from typing import List
 
-def list_to_file(list, name):
-#Function to take a list and put the list in a file line-by-line
+class User:
+    def __init__(self, username, password, hash):
+        self.username = []
+        self.password = []
+        self.hash = []
+
+
+def list_to_file(list, name): #done
+#Function to take a list and puth the list in a file line-by-line
     file = open( name+'.cleaned', 'w')
     for items in list:
         file.write(items+"\n")
     file.close()
     print('Cleaning finished output will be in the file named', name+'.cleaned')    #also might be part of the problem
 
-def Rem_Duplicates(hashes):
+def Rem_Duplicates(hashes): #done
 # Function ot remove the duplicates from a list
     hashes = list(dict.fromkeys(hashes))
     return (hashes)
 
-def File_Clenser(input):                    # Apperently this function isn't using the regular expression needed for DCC2 hashes
+def File_Clenser(input):            # might want to make sure that I can get NTLM hashes from this function
 #Function to go through a file of DCC2 hashes and clean them up
     hash_list = []          # list of uncleaned hashes
-    cleaned_hashes = []     # list of cleaned hashes
-    tmp_hashes = []         # temporary placeholder
     
     # Checking to make sure the file exists
     if (os.path.exists(input) == True):
         # open the file and add each hash into a list
         print('Starting Clense...')         #I'm assuming that this is part of the problem for the printing
-        with open(input) as file:
+        with open(input) as file: 
             hash_list = file.readlines()
-        # going through the list and cleaning the hashes
-        for x in range(len(hash_list)):
-            hash = hash_list[x]
-            tmp = hash[hash.find('$'):]
-            tmp_hashes.append(tmp)
-            # this bit removes the newline characters
-            cleaned_hashes = Remove_null(tmp_hashes)
-
-        # go through the list and remove the duplicates
-        cleaned_hashes = Rem_Duplicates(cleaned_hashes)
+        # clean the hashes from the list
+        List_Cleanser(hash_list, 'tmp')
         # closing the file
         file.close()
-        # send the list to the 'list_to_file' function
-        list_to_file(cleaned_hashes, input)
     # just in case you put in the wrong file name
     else:  
         print('file does NOT exist')
@@ -54,7 +50,6 @@ def Secrets_Parser(input, pattern):
         with open(input) as file:
             content = file.readlines()
         # Pull the hashes
-        print('here')
         hashes = Pattern_Parser(content, pattern) 
 
     #might need the below code incase I break Something
@@ -65,23 +60,12 @@ def Secrets_Parser(input, pattern):
 
     return hashes
 
-def List_Cleanser (hash_list, input):
+def List_Cleanser (hash_list, input):       # might need to make sure that this function can parse NTLM
 # Function to clean a list of dcc2 hashes
     tmp_hashes = []         # temporary placeholder
-
-    for x in range(len(hash_list)):
-        hash = hash_list[x]
-        tmp = hash[hash.find('$'):]
-        tmp_hashes.append(tmp)
-        # this bit removes the newline characters
-        cleaned_hashes = Remove_null(tmp_hashes)
-
-    # Keeping the code below just in case I messed this up somewhere
-#        cleaned_hashes = [
-#            item.replace('\r', '').replace('\n','')
-#            for item in tmp_hashes              
-#        ]
-
+    tmp_hashes = Pattern_Parser (hash_list, pattern_DCC2)
+    # go through the list and remove null bytes
+    cleaned_hashes = Remove_null(tmp_hashes)
     # go through list and remove the duplicates
     cleaned_hashes = Rem_Duplicates(cleaned_hashes)
     # send the list to the 'list_to_file' function
@@ -102,7 +86,7 @@ def Directory_Parser(pattern, directory, mode):
             list_to_file(hashes2, 'secretdumps.NTLM')
 
 def Match(hashes, cracked, pattern_HASH, pattern_CRACKED):
-# This function will take the file of cracked passwods from hashcat and the NTLM files and match the username to the password
+# This function will take the file of cracked passwords from hashcat and the NTLM files and match the username to the password
     hashes_from_file = []
     cracked_from_file = []
     if (os.path.isfile(hashes) == True and os.path.isfile(cracked) == True):
@@ -124,7 +108,7 @@ def Match(hashes, cracked, pattern_HASH, pattern_CRACKED):
     else:
         print('Cracked hash or Hash file does not exist, please check the filename')
 
-def Remove_null(list):
+def Remove_null(list): # done
 # This function removes the null byte at the end of an item in a list
     list = [
         item.replace('\r', '').replace('\n','') for item in list              
@@ -132,16 +116,17 @@ def Remove_null(list):
     return list
 
 def Pattern_Parser(list, pattern):
-# This function SHOULD go through a list and parse based on the regex pattern (MIGHT NOT WORK PROPERLY ATM)
+# this function SHOULD go through a list and parse based on the regex pattern (MIGHT NOT WORK PROPERLT ATM)
 ############################################################
-    class User:
-        def __init__(self, username, password, hash):
-            self.username = []
-            self.password = []                                  #this section is in the wrong function but will fix later
-            self.hash = []
-    Users = [
-        User("mary", "Password1!", "somerandomhash")
-    ]
+#    class User:
+#        def __init__(self, username, password, hash):
+#            self.username = []
+#            self.password = []                                  #this section is in the wrong function but will fix later
+#            self.hash = []
+#    Users = [
+#        User("mary", "Password1!", "somerandomhash")
+#    ]
+    new_user = User("mary", "Password1!", "somerandomhash")
 ############################################################
 
     hashes = []
@@ -162,11 +147,15 @@ def Pattern_Parser(list, pattern):
 #        for match in hash:
             #hashes.append(match.group())
 #            list.append(match.group())
-    for user in Users:
-        print(user.username)
-        print(user.password)
-        print(user.hash)
+    print(new_user.username)
+    print(new_user.password)
+    print(new_user.hash)
     return hashes
+#    for user in Users:
+#        print(user.username)
+#        print(user.password)
+#        print(user.hash)
+#    return hashes
         
 # For the regex for the matching function
 #   Hashes
@@ -176,8 +165,11 @@ def Pattern_Parser(list, pattern):
 #   cracked
 #       group 1: hash
 #       group 2: password
+
     
 if __name__ == "__main__":
+    # Userlist for the matching part of the program
+    userList: List[User] = []
     # Argument parcer stuff
     parser = argparse.ArgumentParser(description='Hash cleaner for the Domain Caches Credentials 2 hash type')
     parser.parse_args
@@ -222,11 +214,13 @@ if __name__ == "__main__":
         File_Clenser(args.FILE)
 
 
+    newUser = User("mary", "Password1!", "somerandomhash")
+    userList.append(newUser)
+    print(userList[0].hash)
+    print(userList[0].username)
+    print(userList[0].password)
+
     #currently need to fix the bug where it prints multiple lines when parsing through the files
     #   I could create another function that will tell the user that the program is finished
-    #could also add an option to match all of the cracked hashes to the users that they belong to
-    #   look in the discord to find the example file to write the code for
-    #   also need to make some new regex to parse the hashed and passwords
-    #there is also an issue with the regular expression for the DCC2 hashes where it grabs everything with "$"
-    #   maybe
-    #   Correction, there is a problem with the file cleanser function where it isn't using the regular expression
+    #could also add an option to match all of the cracked hashes to the users that they belong to\
+    #   most of the code is working just need to work on the matching functions
